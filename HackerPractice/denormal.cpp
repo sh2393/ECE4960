0741d78aa0b382eef82b5c17b6c50bc1aabc2a76
sh2393 <sh2393@cornell.edu>
2019-01-31 11:06:57 -0500

    notes2 finished

---
+++ HackerPractice/denormal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <float.h>
+using namespace std;
+#include <tgmath.h>
+
+int main(){
+	double x = 1.234567890123456789;
+	int i = 1;
+
+	x *= pow(10, -307);
+	for(i = 1; i < 20; i++){
+		x/=10.0;
+		printf("%.16e\n", x);
+	}
+}
+
+//platform does not support denormal
---
+++ HackerPractice/infinity.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <float.h>
+using namespace std;
+
+
+
+bool isINF(double x){
+	if (x > DBL_MAX && (1.0/x == 0)) return true;
+	else return false;
+}
+
+bool isNINF(double x){
+	if (x < DBL_MAX && (1.0/x == 0)) return true;
+	else return false;
+}
+
+bool isNaN(double x){
+	return true;
+}
+
+bool isPositiveZero(double x){
+	if(isINF(1.0/x)) return true;
+	else return false;
+}
+
+bool isNegativeZero(double x){
+	if(isNINF(1.0/x)) return true;
+	else return false;
+}
+
+bool isDoubleMax(double x){
+	if (x == DBL_MAX) return true;
+	else return false;
+}
+
+int main(){
+	double x = DBL_MAX;
+	double xx = x*x;
+	cout << isINF(xx) << endl;
+
+	return 0;
+}
